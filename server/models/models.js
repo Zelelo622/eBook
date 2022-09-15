424e8738b80ba0c3abd098188290b1d51a38cad1
Egor Kirin <egorcom27@gmail.com>
2022-09-15 21:51:03 +0300

    first commit

---
+++ server/models/models.js
@@ -0,0 +1,51 @@
+const sequelize = require('../db');
+const { DataTypes } = require('sequelize');
+const { authenticate, model } = require('../db');
+
+const User = sequelize.define('user', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING }
+});
+
+const Book = sequelize.define('book', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: { type: DataTypes.STRING, allowNull: false },
+    publication_date: { type: DataTypes.DATE },
+    link_file: { type: DataTypes.STRING }
+});
+
+const Bookmark = sequelize.define('bookmark', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    page_num: { type: DataTypes.INTEGER }
+});
+
+const Author = sequelize.define('author', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    first_name: { type: DataTypes.STRING, allowNull: false },
+    last_name: { type: DataTypes.STRING, allowNull: false }
+});
+
+const BookAuthor = sequelize.define('bookAuthor', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+User.hasMany(Bookmark);
+Bookmark.belongsTo(User);
+
+Book.hasMany(Bookmark);
+Bookmark.belongsTo(Book);
+
+Book.hasMany(BookAuthor);
+BookAuthor.belongsTo(Book);
+
+Author.hasMany(BookAuthor);
+BookAuthor.belongsTo(Author);
+
+module.exports = {
+    User,
+    Book,
+    Bookmark,
+    BookAuthor,
+    Author
+}
