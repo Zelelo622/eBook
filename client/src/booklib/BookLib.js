4ebe43cf501133975c21b961a8a3e7dd3a80ba6e
Egor Kirin <egorcom27@gmail.com>
2022-09-29 11:47:53 +0300

    form registration

--- client/src/booklib/BookLib.js
+++ client/src/booklib/BookLib.js
@@ -3,9 +3,9 @@ import { makeAutoObservable } from "mobx";
 export default class BookLib {
     constructor() {
         this._books = [
-            { id: 1, title: 'Искусство войны', publication_date: 2000 - 01 - 01, link_file: 'google.com' },
-            { id: 1, title: 'Удушье', publication_date: 2000 - 01 - 01, link_file: 'google.com' },
-            { id: 1, title: 'На дороге', publication_date: 2000 - 01 - 01, link_file: 'google.com' },
+            { id: 1, title: 'Искусство войны', publication_date: '2000-01-01', link_file: 'google.com' },
+            { id: 1, title: 'Удушье', publication_date: '2000-01-01', link_file: 'google.com' },
+            { id: 1, title: 'На дороге', publication_date: '2000-01-01', link_file: 'google.com' },
         ]
         makeAutoObservable(this);
     }
